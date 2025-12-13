4dc7f4729fe9e05bbba2cb7bcb23467177ffa141
ET06731 <1107692134@qq.com>
2025-12-13 13:20:56 +0800

    支持多LLM 提供商

--- backup/src/background.js
+++
@@ -1,3 +0,0 @@
-// This is a dummy background script for Zotero 7 WebExtension compatibility
-// Although bootstrapped, defining it in manifest helps some validators pass
-console.log("Paper2Slide: Background script loaded (stub)");
